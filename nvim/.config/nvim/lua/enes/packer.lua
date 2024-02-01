347c52b1981db4a151172b2bc683f50c1328085a
eneserciyes <m.eneserciyes@gmail.com>
2024-02-01 00:32:18 -0500

    remove DAP

--- nvim/.config/nvim/lua/enes/packer.lua
+++ nvim/.config/nvim/lua/enes/packer.lua
@@ -77,32 +77,32 @@ return require('packer').startup(function(use)
   use("folke/zen-mode.nvim")
 
   -- Nvim DAP
-  use 'mfussenegger/nvim-dap'
-  use {
-      "rcarriga/nvim-dap-ui",
-      requires = { "mfussenegger/nvim-dap" },
-      config = function()
-          local dap = require("dap")
-          local dapui = require("dapui")
-          dapui.setup()
-          dap.listeners.after.event_initialized["dapui_config"] = function()
-              dapui.open()
-          end
-          dap.listeners.before.event_terminated["dapui_config"] = function()
-              dapui.close()
-          end
-          dap.listeners.before.event_exited["dapui_config"] = function()
-              dapui.close()
-          end
-      end
-  }
-  use {
-      "mfussenegger/nvim-dap-python",
-      requires = { "mfussenegger/nvim-dap", "rcarriga/nvim-dap-ui" },
-      config = function()
-          local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-          require("dap-python").setup(path)
-      end
-  }
+  -- use 'mfussenegger/nvim-dap'
+  -- use {
+  --     "rcarriga/nvim-dap-ui",
+  --     requires = { "mfussenegger/nvim-dap" },
+  --     config = function()
+  --         local dap = require("dap")
+  --         local dapui = require("dapui")
+  --         dapui.setup()
+  --         dap.listeners.after.event_initialized["dapui_config"] = function()
+  --             dapui.open()
+  --         end
+  --         dap.listeners.before.event_terminated["dapui_config"] = function()
+  --             dapui.close()
+  --         end
+  --         dap.listeners.before.event_exited["dapui_config"] = function()
+  --             dapui.close()
+  --         end
+  --     end
+  -- }
+  -- use {
+  --     "mfussenegger/nvim-dap-python",
+  --     requires = { "mfussenegger/nvim-dap", "rcarriga/nvim-dap-ui" },
+  --     config = function()
+  --         local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+  --         require("dap-python").setup(path)
+  --     end
+  -- }
 end)
 
