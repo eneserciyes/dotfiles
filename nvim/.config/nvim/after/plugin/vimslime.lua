69e2de8d41478cd01a25998b6f2fffcadce644c8
eneserciyes <m.eneserciyes@gmail.com>
2024-02-03 16:37:37 -0500

    send whole buffer keymap

--- nvim/.config/nvim/after/plugin/vimslime.lua
+++ nvim/.config/nvim/after/plugin/vimslime.lua
@@ -5,3 +5,5 @@ vim.g.slime_default_config = {socket_name = 'default', target_pane = "{last}"}
 vim.g.slime_python_ipython = 1
 
 vim.g.slime_dont_ask_default = 1
+
+vim.keymap.set("n", "<C-c><C-x>", ":%SlimeSend<CR>")
