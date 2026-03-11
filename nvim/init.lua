d1fc09581d2e4da46e7a92035cc264d32e50523b
eneserciyes <m.eneserciyes@gmail.com>
2026-03-10 23:54:41 -0700

    nvim setup

---
+++ nvim/init.lua
@@ -0,0 +1,66 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.ignorecase = true
+vim.o.wrap = false
+vim.o.winborder = "rounded"
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.smartindent = true
+vim.o.termguicolors = true
+vim.o.swapfile = false
+vim.o.signcolumn = "yes"
+vim.o.undofile = true
+vim.o.incsearch = true
+vim.o.hlsearch = false
+
+local map = vim.keymap.set
+
+vim.g.mapleader = " "
+map('n', '<leader>o', ':update<CR> :source<CR>')
+map('n', '<leader>w', ':write<CR>')
+map('n', '<leader>q', ':quit<CR>')
+map({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+map({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+map({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')
+map({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')
+
+vim.cmd(":hi statusline guibg=None")
+
+-- Plugins
+vim.pack.add({
+	{ src = "https://github.com/vague-theme/vague.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/nvim-mini/mini.pick" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+})
+
+require("vague").setup({ transparent = true })
+vim.cmd('colorscheme vague')
+
+require("mason").setup()
+require("mini.pick").setup()
+require("oil").setup()
+require("nvim-treesitter.config").setup({
+	ensure_installed = { "python", "lua" },
+	highlight = { enable = true }
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd("set completeopt+=noselect")
+
+
+vim.lsp.enable({ "lua_ls", "basedpyright" })
+
+map('n', '<leader>lf', vim.lsp.buf.format)
+map('n', '<leader>f', ':Pick files<CR>')
+map('n', '<leader>h', ':Pick help<CR>')
+map('n', '<leader>e', ':Oil<CR>')
