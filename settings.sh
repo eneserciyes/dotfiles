82dbbff4d993f83edabbc793e4c0bba282ecebab
eneserciyes <m.eneserciyes@gmail.com>
2026-03-28 20:33:42 -0700

    update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,1 +1,14 @@
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write -g ApplePressAndHoldEnabled -bool false
+sudo defaults write com.apple.universalaccess reduceMotion -bool true
+sudo defaults write com.apple.universalaccess reduceTransparency -bool true
+
+
+# show Library folder
+chflags nohidden ~/Library
+
+# show hidden files
+defaults write com.apple.finder AppleShowAllFiles YES
+
+# add pathbar to title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
