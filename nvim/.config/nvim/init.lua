812ab708f7644ba6097567afaef87e1b15be5e31
eneserciyes <m.eneserciyes@gmail.com>
2026-04-04 23:00:50 -0700

    init lua reorg, no clipboard protection

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,7 +6,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- No nerd font	
+-- No nerd font
 vim.g.have_nerd_font = false
 
 -- Line numbers
@@ -30,6 +30,12 @@ vim.o.ttimeoutlen = 10
 -- no mouse
 vim.o.mouse = ""
 
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+
 -- swapfile is annoying only
 vim.o.swapfile = false
 
@@ -49,10 +55,110 @@ vim.o.incsearch = true
 vim.o.autoread = true
 vim.api.nvim_create_autocmd("FocusGained", { command = "checktime" })
 
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+vim.o.confirm = true
+
 -- Set statusline high and no background
 vim.cmd(":hi statusline guibg=None")
 
+----------------------------------------------
+---  REMAPS
+----------------------------------------------
 local map = vim.keymap.set
+
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+map('n', '<leader>lf', function()
+	vim.lsp.buf.code_action({ context = { only = { "source.organizeImports.ruff" } }, apply = true })
+	vim.defer_fn(function()
+		vim.lsp.buf.format()
+		vim.cmd("write")
+	end, 100)
+end)
+map('n', '<leader>f', ':Pick files tool=\'git\'<CR>')
+map('n', '<leader>b', ':Pick buffers<CR>')
+map('n', '<leader>rg', ':Pick grep_live<CR>')
+map('n', '<leader>h', ':Pick help<CR>')
+map('n', '<leader>k', ':Pick keymaps<CR>')
+map('n', '<leader>p', ':Pick oldfiles<CR>')
+map('n', '<leader>e', ':Oil<CR>')
+
+map('n', '<leader>o', ':update<CR> :source<CR>')
+map('n', '<leader>w', ':write<CR>')
+map({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+map({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+map({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')
+map({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')
+map({ "n", "t" }, "<Leader>x", "<Cmd>tabclose<CR>")
+
+vim.cmd([[
+nnoremap g= g+| " g=g=g= is less awkward than g+g+g+
+noremap! <c-r><c-d> <c-r>=strftime('%F')<cr>
+noremap! <c-r><c-t> <c-r>=strftime('%T')<cr>
+noremap! <c-r><c-f> <c-r>=expand('%:t')<cr>
+noremap! <c-r><c-p> <c-r>=expand('%:p')<cr>
+xnoremap <expr> . "<esc><cmd>'<,'>normal! ".v:count1.'.<cr>'
+]])
+
+map("n", "gh", "0", { desc = "Jump: Start of line" })
+map("n", "gl", "$", { desc = "Jump: End of line" })
+vim.keymap.set("n", "yab", ":%y<CR>", { silent = true })
+vim.keymap.set("n", "vab", "ggVG", { noremap = true, silent = true })
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
+vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })
+
+map({ "n", "v", "x" }, "<C-s>", [[:s/\V]], { desc = "Enter substitue mode in selection" })
+map({ "v", "x", "n" }, "<C-y>", '"+y', { desc = "System clipboard yank." })
+map({ "n" }, "<leader>c", "1z=")
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function() vim.hl.on_yank() end,
+})
+
+----------------------------------------------
+---  LSP & DIAGNOSTICS
+----------------------------------------------
+vim.diagnostic.config {
+	update_in_insert = false,
+	severity_sort = true,
+	float = { border = 'rounded', source = 'if_many' },
+	underline = { severity = { min = vim.diagnostic.severity.WARN } },
+
+	-- Can switch between these as you prefer
+	virtual_text = true, -- Text shows up at the end of the line
+	virtual_lines = false, -- Text shows up underneath the line, with virtual lines
+
+	-- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+	jump = { float = true },
+}
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map('n', '<leader>lt', function()
+	local clients = vim.lsp.get_clients()
+	if #clients > 0 then
+		vim.lsp.stop_client(clients)
+		print("LSP stopped")
+	else
+		vim.lsp.enable(lsp_servers)
+		print("LSP started")
+	end
+end)
+
+
 
 -- Plugins
 vim.pack.add({
@@ -187,65 +293,3 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 local lsp_servers = { "lua_ls", "basedpyright", "clangd", "ruff" }
 vim.lsp.enable(lsp_servers)
-
-map('n', '<leader>lt', function()
-	local clients = vim.lsp.get_clients()
-	if #clients > 0 then
-		vim.lsp.stop_client(clients)
-		print("LSP stopped")
-	else
-		vim.lsp.enable(lsp_servers)
-		print("LSP started")
-	end
-end)
-
--- Mappings
-map('n', '<leader>lf', function()
-	vim.lsp.buf.code_action({ context = { only = { "source.organizeImports.ruff" } }, apply = true })
-	vim.defer_fn(function()
-		vim.lsp.buf.format()
-		vim.cmd("write")
-	end, 100)
-end)
-map('n', '<leader>f', ':Pick files tool=\'git\'<CR>')
-map('n', '<leader>b', ':Pick buffers<CR>')
-map('n', '<leader>rg', ':Pick grep_live<CR>')
-map('n', '<leader>h', ':Pick help<CR>')
-map('n', '<leader>k', ':Pick keymaps<CR>')
-map('n', '<leader>p', ':Pick oldfiles<CR>')
-map('n', '<leader>e', ':Oil<CR>')
-
-map('n', '<leader>o', ':update<CR> :source<CR>')
-map('n', '<leader>w', ':write<CR>')
-map('n', '<leader>q', ':quit<CR>')
-map({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
-map({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
-map({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')
-map({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')
-map({ "n", "t" }, "<Leader>x", "<Cmd>tabclose<CR>")
-
-vim.cmd([[
-nnoremap g= g+| " g=g=g= is less awkward than g+g+g+
-noremap! <c-r><c-d> <c-r>=strftime('%F')<cr>
-noremap! <c-r><c-t> <c-r>=strftime('%T')<cr>
-noremap! <c-r><c-f> <c-r>=expand('%:t')<cr>
-noremap! <c-r><c-p> <c-r>=expand('%:p')<cr>
-xnoremap <expr> . "<esc><cmd>'<,'>normal! ".v:count1.'.<cr>'
-]])
-
-map("n", "gh", "0", { desc = "Jump: Start of line" })
-map("n", "gl", "$", { desc = "Jump: End of line" })
-vim.keymap.set("n", "yab", ":%y<CR>", { silent = true })
-vim.keymap.set("n", "vab", "ggVG", { noremap = true, silent = true })
-
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
-vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
-vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })
-
-map({ "n", "v", "x" }, "<C-s>", [[:s/\V]], { desc = "Enter substitue mode in selection" })
-map({ "v", "x", "n" }, "<C-y>", '"+y', { desc = "System clipboard yank." })
-map({ "n" }, "<leader>c", "1z=")
